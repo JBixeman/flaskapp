a7c3d6401deb62db1d24eb0bf6db64a72db10717
JBixeman <bixeman@icloud.com>
2025-05-12 23:03:48 -0500

    Added a lengthy form and a thankyou page

--- basic.py
+++ basic.py
@@ -1,25 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import (StringField, SubmitField, BooleanField,DateTimeField,
+                     RadioField,SelectField,
+                     TextAreaField,TextField)
+
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '<KEY>'
+app.config['SECRET_KEY'] = 'KEY'
 
 class InfoForm(FlaskForm):
 
-    breed = StringField('What breed are you?')
+    breed = StringField('What breed are you?', validators=[DataRequired()])
+    neutered = BooleanField('Neutered?')
+    mood = RadioField('Mood?', choices=[('mood_one','Happy'),('mood_two','Happy'),('mood_three','Excited')])
+    food_choice = SelectField(u'Pick your favorite food:',choices=[('chi','Chicken'),('bf','Beef'),('fish','Fish')])
+    feedback = TextAreaField('Feedback:')
     submit = SubmitField('Submit')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    breed = False
     form = InfoForm()
     if form.validate_on_submit():
-        breed = form.breed.data
-        form.breed.data = ''
-    return render_template('base.html', form=form, breed=breed)
+        session['breed'] = form.breed.data
+        session['neutered'] = form.neutered.data
+        session['mood'] = form.mood.data
+        session['food_choice'] = form.food_choice.data
+        session['feedback'] = form.feedback.data
+
+        return redirect(url_for('thankyou'))
+    return render_template('base.html', form=form)
+
+
+@app.route('/thankyou')
+def thankyou():
+    return render_template('thankyou.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
